78d8b0d04e06f5cfaaa86c61b4c36bed963a6283
danik <me@subsonic.cz>
2017-02-10 00:09:35 +0100

    Refactoring ahead of 2.0

--- loader.js
+++
@@ -1,6 +0,0 @@
-/* jahudka/minify compatible loader */
-/*! @include file="src/js/Nittro/DI/ServiceDefinition.js" */
-/*! @include file="src/js/Nittro/DI/ContainerMixin.js" */
-/*! @include file="src/js/Nittro/DI/Container.js" */
-/*! @include file="src/js/Nittro/DI/BuilderExtension.js" */
-/*! @include file="src/js/Nittro/DI/ContainerBuilder.js" */
